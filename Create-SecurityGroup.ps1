3e87e1fc6b894b08f73035d51eefbcf81db85a40
DanZab <D.Zabinski@gmail.com>
2020-09-06 17:18:42 -0400

    Add files via upload

---
+++ Create-SecurityGroup.ps1
@@ -0,0 +1,189 @@
+﻿<#
+    .SYNOPSIS
+        Creates AD Security Groups and adds users
+
+    .DESCRIPTION
+        Requires Active Directory PS Modules
+
+        This script does the following:
+        1. Prompts for Group Name, Description, Owner and Location
+        2. After confirmation, creates the group
+        3. Prompts for users to add to the group once it's created
+        4. Copies a note to the clipboard to paste into ticket close notes
+
+        The Group Owner, and Task Number (for ticketing system) are added to the group
+        notes section after the group has been created.
+
+    .NOTES 
+        Author: Dan Zabinski https://github.com/DanZab
+        Date: 8/30/2020
+
+#>
+
+Clear-Host
+
+# Generates the menu for selecting group location, stored in a function to be easily updated
+Function Show-PathMenu {
+    Write-Host "`n"
+    Write-Host -ForegroundColor Green "=== Available Security Group OUs ==="
+
+    #  OU=Applications,OU=Groups,DC=domain,DC=local
+    Write-Host -ForegroundColor Green "   1: Groups \ Applications"
+
+    #  OU=App1,OU=Applications,OU=Groups,DC=domain,DC=local
+    Write-Host -ForegroundColor Green "   2: Groups \ Applications \ App1"
+
+    #  OU=App2,OU=Applications,OU=Groups,DC=domain,DC=local
+    Write-Host -ForegroundColor Green "   3: Groups \ Applications \ App2"
+
+    #  OU=Group Policy,OU=Groups,DC=domain,DC=local
+    Write-Host -ForegroundColor Green "   4: Groups \ Group Policy"
+
+    Write-Host "`n"
+}
+
+####################################################################
+# Step 1 - Prompts for Group Name, Description, Owner and Location 
+####################################################################
+
+Write-host "Please enter the task or request number: " -ForegroundColor Yellow -NoNewLine
+$TaskNumber = Read-host
+
+Write-host "What is the name of the group: " -ForegroundColor Yellow -NoNewLine
+$GroupName = Read-Host
+
+Write-host "What is the group description (ex. Grants access to...): " -ForegroundColor Yellow -NoNewLine
+$description = Read-Host
+
+Write-host "Who is the group owner: " -ForegroundColor Yellow -NoNewLine
+$owner = Read-Host
+
+$Notes = "Owner $owner, $TaskNumber"
+
+Show-PathMenu
+Write-host "Please enter the number for the OU where you would like to create the group: " -ForegroundColor Yellow -NoNewLine
+$selection = Read-Host
+switch ($selection)
+{
+    '1' 
+    { $path = "OU=Applications,OU=Groups,DC=domain,DC=local"}
+    '2'
+    { $path = "OU=App1,OU=Applications,OU=Groups,DC=domain,DC=local" }
+    '3'
+    { $path = "OU=App2,OU=Applications,OU=Groups,DC=domain,DC=local" }
+    '4'
+    { $path = "OU=Group Policy,OU=Groups,DC=domain,DC=local" }
+    default
+    { Write-host -ForegroundColor Red "No option select, exiting script."; Exit}
+}
+
+####################################################################
+# Step 2 - After confirmation, creates the group
+# Displays the information and the powershell command used to create the group
+####################################################################
+
+Write-host "`n"
+Write-host "Group Name:" -ForegroundColor Yellow -NoNewLine
+Write-host " $GroupName"
+Write-host "Group Description:" -ForegroundColor Yellow -NoNewLine
+Write-host " $description"
+Write-host "Group Notes: " -ForegroundColor Yellow -NoNewLine
+Write-host "$Notes"
+Write-host "Group Location:" -ForegroundColor Yellow -NoNewLine
+Write-host " $path"
+Write-host "`n"
+
+Write-host "Is this information correct? Enter " -ForegroundColor Yellow -NoNewLine
+Write-host "Y" -ForegroundColor Green -NoNewLine
+Write-host " to coninue: " -ForegroundColor Yellow -NoNewLine
+$continue = Read-Host
+
+If ($continue -eq "Y") {
+
+Write-host "`n"
+Write-Host -ForegroundColor Yellow "New-ADGroup -Name ""$GroupName"" -SamAccountName ""$GroupName"" -GroupCategory Security -GroupScope Global -DisplayName ""$GroupName"" -Path ""$path"" -Description ""$description"" -OtherAttributes @{'info'=""$Notes""}"
+
+
+Try {
+    New-ADGroup -Name "$GroupName" -SamAccountName "$GroupName" -GroupCategory Security -GroupScope Global -DisplayName "$GroupName" -Path "$path" -Description "$description" -OtherAttributes @{'info'="$Notes"}
+
+    Write-host "`n"
+    Write-Host -ForegroundColor Green "New Group Created."
+
+
+    $skip = 0
+    }
+Catch {
+    Write-Host -ForegroundColor Red "Failed to create group."
+    Write-Host -ForegroundColor Red "$($_.Exception.Message)"
+
+    $skip = 1
+    }
+
+    Write-host "`n"
+
+####################################################################
+# Step 3 - Prompts for users to add to the group once it's created
+####################################################################
+    if ($skip -eq 0) {
+
+        # Begin Section to add users to the new Security Group
+        $userCount = 0
+
+        Write-host "Enter user(s) to add to group. (" -ForegroundColor Yellow -NoNewLine
+        Write-host "X" -ForegroundColor Green -NoNewLine
+        Write-host " or " -ForegroundColor Yellow -NoNewLine
+        Write-host "[Blank]" -ForegroundColor Green -NoNewLine
+        Write-host " to end.) " -ForegroundColor Yellow
+
+        Do {
+            Write-host "User $($userCount + 1): " -ForegroundColor Yellow -NoNewLine
+            # Prompt for usernames, remove tabs and spaces
+            $user = Read-Host
+            $user = $user.Trim("`t"," ")
+                
+            If (($user -ne "X") -and ($user -ne "")) {
+                Try {
+                    $ADUser = Get-ADUser $user
+                    Add-ADGroupMember $GroupName -Members $ADUser.name
+
+                    $userCount = $userCount + 1
+                }
+                Catch {
+                    Write-Host -ForegroundColor Red "User not found."
+                    Write-Host -ForegroundColor Red "$($_.Exception.Message)"
+                }
+            }
+
+        } While (($user -ne "X") -and ($user -ne ""))
+        
+        if ($userCount -gt 0) {
+            Write-host "`n"
+            Write-host "The following users are members of the new $GroupName group:" -ForegroundColor Green
+            Get-ADGroupMember $GroupName | Select Name | Format-Table -AutoSize
+            
+            if ($userCount -eq 1) {
+                $EndStatement = ", $user added."
+            } else {
+                $EndStatement = ", users added."
+            }
+        } else {
+            $EndStatement = "."
+        }
+
+####################################################################
+# Step 4 - Copies a note to the clipboard to paste into ticket close notes
+####################################################################
+        $Statement = "Group $GroupName created" + $EndStatement
+        Write-Host -ForegroundColor Green "Saved to clipboard: " -NoNewline
+        Write-Host $Statement
+        Set-Clipboard -Value $Statement
+
+        Write-host "`n"
+    }
+    
+} Else {
+
+Write-host -ForegroundColor Red "Action cancelled, no group was created"
+
+}
---
+++ Script-Components.ps1
@@ -0,0 +1,172 @@
+﻿<#
+    .DESCRIPTION
+        This is a collection of useful components I've accumulated over time that I
+        incorporate into other scripts or daily use.
+
+        It includes the following elements:
+
+        1. Write Status
+           I use this function on larger scripts to easily format status messages I want
+           displayed in the console. Replaces 'Write-Host' with 'Write-Status'.
+
+        2. Input file prompt
+           Displays each file from an 'input' subdirectory from where the script runs.
+           Allows user to easily select the input file they want.
+
+        3. Quick Input
+            This is a script template I frequently have open in the ISE console. It
+            allows you to quickly enter a list of objects, separated by lines, that are
+            then turned into an array you can iterate on.
+
+            This saves the trouble of having to create an input file to run an action
+            on a list of objects.
+
+        4. Write-Progress Example
+            I use the Write-Progress command frequently to display a progress bar on the
+            script I'm running. I like to keep this example handy to copy/paste into
+            scripts rather than look it up each time.
+
+    .NOTES 
+        Author: Dan Zabinski https://github.com/DanZab
+        Date: 8/30/2020
+
+#>
+
+#####################################################################################
+#   Fn Write-Status
+#####################################################################################
+Function Write-Status ($Message){
+    Write-Host `n -NoNewline
+    Write-Host -ForegroundColor Black -BackgroundColor White "   $Message   "
+}
+
+<#
+#####################################################################################
+#   Fn Get-InputFile
+#####################################################################################
+
+    Returns a value containing the path to the selected Input file.
+
+    Can update $InputFolderPath if necessary, by default it uses the input subdirectory
+    from the location the script is ran from.
+
+    Example of use (if input file was in CSV form):
+        $InputFile = Get-InputFile
+        $MyInputArray = Import-CSV $InputFile
+#>
+
+Function Get-InputFile(){
+    $InputFolderPath = "$((Get-Location).Path)\input"
+    Try {
+        $FileList = Get-ChildItem $InputFolderPath
+    }
+    Catch {
+        Write-Host -ForegroundColor Red "Input folder not found at " -NoNewline
+        Write-Host -ForegroundColor Yellow $InputFolderPath
+
+        $FileList = "None"
+    }
+
+    Do {
+        If ($FileList -ne "none") {
+            Write-Host "Pleaes choose your input file: "
+            For ($i=0; $i -le ($FileList.Count - 1); $I++){
+                Write-Host -BackgroundColor White " " -NoNewline
+                Write-Host -ForegroundColor Black -BackgroundColor White ($i + 1) -NoNewline
+                Write-Host -BackgroundColor White " " -NoNewline
+                Write-Host " " -NoNewline
+                Write-Host $FileList[$i]
+            }
+            Write-Host -ForegroundColor Black -BackgroundColor White " F " -NoNewline
+            Write-Host " to enter custom file path"
+            
+            Write-Host -ForegroundColor Black -BackgroundColor White " X " -NoNewline
+            Write-Host " or " -NoNewline
+            Write-Host -ForegroundColor Black -BackgroundColor White " Exit " -NoNewline
+            Write-Host " to cancel"
+            Write-Host `n
+
+            $UserInput = Read-Host "Please enter your selection"
+        }
+        Else {
+            $UserInput = "F"
+        }
+        
+        Switch -Regex ($UserInput) {
+            '\d+' {
+                [int]$Selection = $UserInput
+                $Selection = $Selection - 1
+                $InputFile = "$($InputFolderPath)\$($FileList[$Selection].Name)"
+
+                If ($Status) {Clear-Variable Status}
+            }
+            "F" {
+                Write-Host "Please enter the full file path for the input file (type " -NoNewline
+                Write-Host -ForegroundColor Yellow "X" -NoNewline
+                Write-Host " or " -NoNewline
+                Write-Host -ForegroundColor Yellow "Exit" -NoNewline
+                Write-Host " to cancel): " -NoNewline
+                $CustomFilePath = Read-Host
+
+                Switch ($CustomFilePath) {
+                    "Exit" {Write-Status "Cancelling script"; Exit}
+                    "X" {Write-Status "Cancelling script"; Exit}
+                    default {
+                        if (Test-Path $CustomFilePath) {
+                            $InputFile = $CustomFilePath
+                            Clear-Variable error
+                        }
+                        else {
+                            Write-Host -ForegroundColor Red "Invalid File Path"
+                            $Status = "error"
+                        }
+                            ; Break}
+                }
+            }
+            "Exit" {Write-Status "Cancelling script"; Exit}
+            "X" {Write-Status "Cancelling script"; Exit}
+            default {$Status = "error"; break}
+        }
+        
+    } While ($Status)
+
+    return $InputFile
+}
+
+#####################################################################################
+# Quick Input
+#####################################################################################
+# Copy/paste your list of objects into the $input variable, then
+# update your ForEach loop with the action you would like to perform.
+
+$input = @'
+DeviceOne
+DeviceTwo
+DeviceThree
+'@
+
+$MyArray = $input -split "`r`n"
+ForEach ($Object in $MyArray) {
+    Get-ADComputer $Object
+}
+
+
+#####################################################################################
+# Write-Progress Example
+#####################################################################################
+
+Write-Host "`n `n `n `n `n"
+Write-Host "Performing the specified task"
+#Get list of objects to perform the action on
+$ObjectList = Import-CSV C:\Temp\Sample.csv
+
+Write-Host "Found $($ObjectList.Count) Objects, performing action"
+
+$i = 1
+ForEach ($Object in $ObjectList) {
+    Write-Progress -Activity "Object $i out of $($ObjectList.Count)" -PercentComplete ($i / $($ObjectList.Count) * 100)
+
+    #Action
+
+    $i ++
+}
